858eb6c22de43c943f6f72baffed12af744d2622
Suryansh Goyal <suryanshg47@gmail.com>
2026-06-30 21:28:05 -0400

    Finalized audit pipeline

--- src/llm_asr_clarification/scripts/clarifications/audit.py
+++ src/llm_asr_clarification/scripts/clarifications/audit.py
@@ -9,32 +9,34 @@ import random
 from llm_asr_clarification.models import OpenAIWrapper
 from llm_asr_clarification.constants import SAMPLE_MEETINGS
 import json
+from typing import List
+import re
 
 
 GROUND_TRUTH_TRANSCRIPT = "parsed_diarized_gt.txt"
 QUIZ_FILE = "quiz_from_parsed_diarized_gt.json"
 
+
+
 TIMEWINDOW_FINDER_SYSTEM_PROMPT = """
 # TASK DESCRIPTION
 You are an expert at analyzing meeting transcripts. You will be given a meeting quiz question, its correct answer, and a chronological Ground Truth meeting transcript. 
-Your task is to identify the line or lines in the transcript where the correct answer is explicitly discussed.
+Your task is to identify the exact line or lines in the transcript where the correct answer is explicitly discussed.
 
 # TRANSCRIPT FORMAT
+Every line follows this exact structure:
 (START_TIME - END_TIME)[SPEAKER_ID]: Spoken Text...
 
 # EXTRACTION RULES
 1. Locate every line in the transcript that directly provides the factual basis for the provided answer.
-2. For each relevant line, extract its exact start time and end time.
+2. You must copy the matching lines completely and VERBATIM. Do not alter the timestamps, speaker IDs, or text.
 
 # OUTPUT FORMAT
-Your response must be a valid JSON array of dictionaries, and NOTHING else. Do not wrap the JSON in markdown code blocks. Each dictionary must contain three keys:
-- "start": The start timestamp integer.
-- "end": The end timestamp integer.
-- "justification": The exact quote from that line.
+Your response must be a valid JSON array of strings, and NOTHING else. Do not wrap the JSON in markdown code blocks. Each string in the array must be an exact copy of a matching transcript line.
 
 Example Output:
 [
-  {"start": 212, "end": 219, "justification": "that yellow stand there represents the the charging stand."}
+  "(1011 - 1016)[Speaker X]: Lorem Ipsum ..."
 ]
 """
 
@@ -50,6 +52,63 @@ TIMEWINDOW_FINDER_USER_PROMPT_TEMPLATE = """
 
 Output:
 """
+
+
+MD_FILE_ITEM_TEMPLATE = """
+## QUESTION
+{quiz_question}
+
+### CORRECT ANSWER
+{correct_answer}
+
+### CORRESPONDING LINES
+#### GROUND TRUTH
+```
+{ground_truth_text}
+```
+
+#### WHISPER
+```
+{whisper_text}
+```
+
+---
+"""
+
+
+def extract_timestamps(line):
+    # Extract all sequences of digits from the string
+    numbers = re.findall(r'\d+', line)
+
+    # Get the first two numbers and convert them to integers
+    start_time = int(numbers[0])
+    end_time = int(numbers[1])
+
+    return start_time, end_time
+
+
+
+def find_relevant_lines_using_ground_truth(transcript: str, gt_lines: List[str]):
+    transcript_lines = transcript.split("\n")
+    overlapping_lines = []
+    for line in transcript_lines:
+        # try:
+        #     start, end = extract_timestamps(line)
+        # except Exception as ex:
+        #     ipdb.set_trace()
+        #     continue
+
+        start, end = extract_timestamps(line)
+
+        for gt_line in gt_lines:
+            gt_start, gt_end = extract_timestamps(gt_line)
+            if start < gt_end and gt_start < end:
+                overlapping_lines.append(line)
+                break
+
+    return overlapping_lines
+
+
 
 # Driver Code
 def run(args_list=None):
@@ -124,14 +183,14 @@ def run(args_list=None):
 
             transcript_path = meeting_folder / "transcripts" / TRANSCRIPT_FILE
             gt_transcript_path = meeting_folder / "transcripts" / GROUND_TRUTH_TRANSCRIPT
-            quiz_path = meeting_folder / "transcripts" / QUIZ_FILE
+            quiz_path = meeting_folder / "quiz" / QUIZ_FILE
 
             # Load all files
             with open(transcript_path, "r") as f:
-                transcript = f.read()
+                whisper_transcript = f.read().strip()
 
             with open(gt_transcript_path, "r") as f:
-                gt_transcript = f.read()
+                gt_transcript = f.read().strip()
 
             with open(quiz_path, "r") as f:
                 quiz = json.load(f)
@@ -141,6 +200,9 @@ def run(args_list=None):
                                  if q[f"score_using_{TRANSCRIPT_FILE.split(".")[0]}"] != 1]
             failed_by_gt = [q for q in quiz 
                             if q[f"score_using_{GROUND_TRUTH_TRANSCRIPT.split(".")[0]}"] != 1]
+            
+            md_file_content = """# FAILED QUIZ BY WHISPER
+            """
             
             # Using an LLM, find the timestamp window in the GT transcript, related to the correct answer
             for failed_question in failed_by_whisper:
@@ -156,11 +218,31 @@ def run(args_list=None):
 
                 # Prompt the LLM
                 response = TIMEWINDOW_FINDER_LLM.prompt_chatgpt(
+                    model = "gpt-5.4-mini",
                     prompt = user_prompt,
-                    max_tokens = 128
+                    max_completion_tokens=1024
                 )
+                
+                # Parse response as JSON
+                try:
+                    relevant_gt_lines = json.loads(response)
+                except Exception as ex:
+                    logger.info(f"Error while parsing {response} as JSON!")
+                    continue
 
-                ipdb.set_trace()
+                # Find corresponding lines from Whisper transcript
+                relevant_whisper_lines = find_relevant_lines_using_ground_truth(whisper_transcript, relevant_gt_lines)
 
+                # Append to md_file_content
+                failed_question_analysis_md = MD_FILE_ITEM_TEMPLATE.format(
+                    quiz_question = question,
+                    correct_answer = correct_answer,
+                    ground_truth_text = "\n".join(relevant_gt_lines),
+                    whisper_text = "\n".join(relevant_whisper_lines)
+                )
+                md_file_content += failed_question_analysis_md
 
-            break
+            # Save the MD file
+            md_file_path = meeting_folder / "quiz" / "failed_quiz_by_whisper.md"
+            with open(md_file_path, "w") as f:
+                f.write(md_file_content)
