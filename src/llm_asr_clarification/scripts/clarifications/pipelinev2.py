059814a47d58519605917e3d587fa54bdc780abb
macmacmacmac <mac.kongsomjit@gmail.com>
2026-08-06 15:05:01 -0400

    added new pipeline

---
+++ src/llm_asr_clarification/scripts/clarifications/pipelinev2.py
@@ -0,0 +1,173 @@
+import os
+import argparse
+from llm_asr_clarification import get_logger
+from pathlib import Path
+from tqdm import tqdm
+from tqdm.contrib.logging import logging_redirect_tqdm
+import ipdb
+import random
+from llm_asr_clarification.models import OracleTranscript, OpenAIWrapper
+from typing import Tuple, List
+import re
+from llm_asr_clarification.constants import SAMPLE_MEETINGS
+
+SAMPLING_RATE = 16_000
+GROUND_TRUTH_TRANSCRIPT = "parsed_diarized_gt.txt"
+
+
+# ┌───────────────────────────────────────────────┐
+# │                   HELPER METHODS              │
+# └───────────────────────────────────────────────┘
+def extract_timestamps(line):
+    # Extract all sequences of digits from the string
+    numbers = re.findall(r'\d+', line)
+
+    # Get the first two numbers and convert them to integers
+    start_time = int(numbers[0])
+    end_time = int(numbers[1])
+
+    return start_time, end_time
+
+# def print_timestamps(idx_pair: Tuple[int], transcript_lines: List[str]):
+#     logger.info("Processing indices")
+#     for idx in idx_pair:
+#         line = transcript_lines[idx]
+#         start_time, end_time = extract_timestamps(line)
+#         logger.info(f"idx: {idx}, line: {line}, start: {start_time}, end: {end_time}")
+#     logger.info("---")
+
+
+
+# Driver Code
+def run(args_list=None):
+    exp_name = os.path.basename(__file__)
+    
+    # Perform CLI Argument Parsing
+    parser = argparse.ArgumentParser()
+    parser.add_argument("--dataset-path", type=str, default="./shared/datasets/amicorpus/train")
+    parser.add_argument("--transcript-file", type=str, default="custom_transcript_gt_segments.txt")
+    parser.add_argument("--do-sample-meetings", type=bool, default=False)
+    parser.add_argument("--strategy", type=str, default="RANDOM")
+    parser.add_argument("--seed", type=int, default=47)
+    
+    args, _ = parser.parse_known_args(args_list)
+
+    # Parse CLI arguments to global variables
+    DATASET_PATH = Path(args.dataset_path)
+    TRANSCRIPT_FILE = args.transcript_file
+    global STRATEGY
+    STRATEGY = args.strategy
+    SEED = args.seed
+
+    random.seed(SEED)
+    
+    # Init Logger
+    global logger
+    logger = get_logger(exp_name)    
+    logger.info(f"{'='*100}\n\t\t\t\tRunning script: {exp_name}\n{'='*100}")
+
+    # Log received args
+    received_args_log = ""
+    for arg, value in vars(args).items():
+        received_args_log += f"|---> {arg}: {value}\n"
+    logger.info(
+        f"Received the following arguments:\n{received_args_log}"
+    )
+
+
+    # ┌───────────────────────────────────────────────┐
+    # │                   LOAD DATA                   │
+    # └───────────────────────────────────────────────┘
+    if args.meeting_name:
+        meeting_folders=[DATASET_PATH / args.meeting_name]
+    else:
+        # Fetch all dataset meeting folders
+        # meeting_folders = [f for f in DATASET_PATH.iterdir() 
+        #                     if (f.is_dir() and 
+        #                         f.name not in ["ami_public_manual_1.6.2", "xinlu_data"])]
+        meeting_folders = [f for f in DATASET_PATH.iterdir()
+                          if (f.is_dir() and
+                              f.name in SAMPLE_MEETINGS)]
+        
+
+    # Wrap logging with tqdm
+    with logging_redirect_tqdm(loggers=[logger]):
+
+        # Process all Meeting Folders
+        for meeting_folder in tqdm(meeting_folders, desc="Processing Meetings"):
+            logger.info(f"Processing meeting {meeting_folder.name}")
+
+            transcript_path = meeting_folder / "transcripts" / TRANSCRIPT_FILE
+            oracle_transcript_path = meeting_folder / "transcripts" / GROUND_TRUTH_TRANSCRIPT
+
+            # Fetch the Oracle Transcript
+            oracle_transcript = OracleTranscript(
+                transcript_path=oracle_transcript_path,
+                logger=logger
+            )
+
+            # Fetch all lines of the oracle transcript
+            ground_truth_lines = oracle_transcript.get_lines()
+
+            # Open and Read the generated transcript
+            with open(transcript_path, "r") as f:
+                transcript_content = f.read()
+
+            # Maintain two lists, one stores original line and one stores updated lines
+            original_transcript_lines = transcript_content.split("\n")
+            updated_transcript_lines = original_transcript_lines.copy()
+
+            # Select 20 random lines
+            random_line_idxs = random.choices(range(0, len(original_transcript_lines)), k = 20)
+            
+            # Group them into 10 Pairs
+            random_idx_pairs = list(zip(random_line_idxs[::2], random_line_idxs[1::2]))
+
+            # For each idx pair
+            for idx_pair in random_idx_pairs:
+                # print_timestamps(idx_pair, original_transcript_lines)
+
+                # Choose 1 from the pair
+                chosen_idx = choose_option(idx_pair, original_transcript_lines, ground_truth_lines)
+                chosen_line = original_transcript_lines[chosen_idx]
+
+                # if meeting_folder.name == "ES2005a":
+                #     ipdb.set_trace()
+
+                # Extract timestamps for the chosen line
+                start_time, end_time = extract_timestamps(chosen_line)
+                
+                # Use Oracle Transcript to fetch timestamp oriented transcription
+                oracle_lines = oracle_transcript.get_oracle_transcription(start_time=start_time, end_time=end_time)
+
+                # Add extracted timestamps to the oracle lines
+                oracle_lines = [f"({start_time} - {end_time}){line}" for line in oracle_lines]
+
+                # Update transcript line with the combined oracle lines
+                updated_transcript_lines[chosen_idx] = "".join(oracle_lines).strip()
+
+
+                logger.info(f"Clarified timestamps: {start_time} - {end_time}")
+
+
+
+            # ┌───────────────────────────────────────────────┐
+            # │                     SAVE                      │
+            # └───────────────────────────────────────────────┘
+            clarified_file_name = TRANSCRIPT_FILE.split(".")[0] + f"_{STRATEGY.lower()}_clarify.txt"
+            fixed_transcript_file_path = meeting_folder / "transcripts" / clarified_file_name
+            with open(fixed_transcript_file_path, "w", encoding="utf-8") as f:
+                f.write("\n".join(updated_transcript_lines))
+                
+            logger.info(f"Saved transcript for {fixed_transcript_file_path}\n\n")
+
+
+
+
+
+
+
+
+
+
+            
