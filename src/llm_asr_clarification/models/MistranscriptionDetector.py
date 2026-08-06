12eb9a17e3e8ad00d123761ae536e9d704343004
Suryansh Goyal <suryanshg47@gmail.com>
2026-08-06 14:15:04 -0400

    Added some minor changes to MistranscriptionDetector

--- src/llm_asr_clarification/models/MistranscriptionDetector.py
+++ src/llm_asr_clarification/models/MistranscriptionDetector.py
@@ -2,18 +2,21 @@ import os
 import ipdb
 from llm_asr_clarification import get_logger
 from transformers import AutoTokenizer
-from pathlib import Path
 import re
 import json
 import pandas as pd
 import torch
 import pickle
-import ipdb
 import numpy as np
 from jiwer import wer
 import jiwer
 from rouge_score import rouge_scorer
+import string
+from abc import ABC, abstractmethod
 
+# ┌───────────────────────────────────────────────┐
+# │               GLOBAL VARIABLES                │
+# └───────────────────────────────────────────────┘
 # Define a robust transformation pipeline
 # This applies data cleaning steps in order, from top to bottom
 JIWER_TRANSFORM = jiwer.Compose([
@@ -23,20 +26,26 @@ JIWER_TRANSFORM = jiwer.Compose([
     jiwer.Strip(),                      # Clean up leading/trailing whitespaces
     jiwer.ReduceToListOfListOfWords()   # Format text tokens perfectly for jiwer's internal engine
 ])
-scorer = rouge_scorer.RougeScorer(
+
+ROUGE_SCORER = rouge_scorer.RougeScorer(
     ["rougeL"],
     # use_stemmer=True,
     use_stemmer=False
 )
 
+LLM_MODEL_NAME = "meta-llama/Llama-3.1-8B-Instruct"   # or another causal LM
+LLM_TOKENIZER = AutoTokenizer.from_pretrained(LLM_MODEL_NAME)
+
+
+# ┌───────────────────────────────────────────────┐
+# │                 HELPER METHODS                │
+# └───────────────────────────────────────────────┘
 def rouge_l(pred, ref):
-    return scorer.score(
+    return ROUGE_SCORER.score(
         ref,   # reference first
         pred   # prediction second
     )["rougeL"]
 
-import re
-import string
 
 def normalize_text(text: str) -> str:
     """Mirrors the jiwer text normalization pipeline, except for last stage"""
@@ -58,70 +67,48 @@ def normalize_text(text: str) -> str:
     
     return text
 
-pd.set_option('display.max_colwidth', None)
-
-MODEL_NAME = "meta-llama/Llama-3.1-8B-Instruct"   # or another causal LM
-tokenizer = AutoTokenizer.from_pretrained(MODEL_NAME)
-scorer = rouge_scorer.RougeScorer(
-    ["rougeL"],
-    # use_stemmer=True,
-    use_stemmer=False
-)
-
-def rouge_l(pred, ref):
-    return scorer.score(
-        ref,   # reference first
-        pred   # prediction second
-    )["rougeL"]
-
-from abc import ABC, abstractmethod
-
-MODEL_NAME = "meta-llama/Llama-3.1-8B-Instruct"   # or another causal LM
-tokenizer = AutoTokenizer.from_pretrained(MODEL_NAME)
 
 class MistranscriptionDetector(ABC):
     @abstractmethod
-    def __init__(
-            self, 
-            **kwargs
-    ):
+    def __init__(self, **kwargs):
         pass
 
     @abstractmethod
-    def pred_mistranscribed(
-        self, line_numbers: list[int]
-    ) -> list[bool]:
+    def pred_mistranscribed(self, line_numbers: list[int]) -> list[bool]:
         pass
+
 
 class RandomBernoulliDetector(MistranscriptionDetector):
     """
-        Trivial baseline detector, returns positive predictions with probability p.
+    Trivial baseline detector, returns positive predictions with probability p.
     """
     def __init__(self, p=0.592):
+        """
+        The default value of p here is taken from data analysis phase, which is the percent of sentences 
+        across the training meetings which are mistranscribed (according to ROUGE-L < 0.5) by whisper_tiny.
+        """
         self.p = p
-    def pred_mistranscribed(
-            self, line_numbers: list[int]
-    ) -> list[bool]:
+
+        
+    def pred_mistranscribed(self, line_numbers: list[int]) -> list[bool]:
         return (np.random.rand(len(line_numbers)) < self.p).tolist()
+
 
 class GTDetector(MistranscriptionDetector):
     """
-        Teacher detector based on GT used to train other models.    
+    Teacher detector based on GT used to train other models.    
     """
-    def __init__(
-            self, 
-            **kwargs
-        ):
+    def __init__(self, **kwargs):
 
         defaults = {
             'meeting_path' : "./shared/datasets/amicorpus/train/ES2005d/",
-            'threshold' : 0.5
+            'rougeL_threshold' : 0.5
         }
 
         params  = {**defaults, **kwargs}
 
         meeting_path = params['meeting_path']
-        threshold = params['threshold']
+        self.rougeL_threshold = params['rougeL_threshold']
 
         # Read the transcript using the provided path
         beam_results = os.path.join(meeting_path, "artifacts", "beam_results.json")
@@ -157,39 +144,36 @@ class GTDetector(MistranscriptionDetector):
         ]
         
         df['num_tokens_text'] = df['beam_1_text'].apply(
-            lambda x: len(tokenizer.encode(x))
+            lambda x: len(LLM_TOKENIZER.encode(x))
         )
         df['num_tokens_gt'] = df['gt'].apply(
-            lambda x: len(tokenizer.encode(x))
+            lambda x: len(LLM_TOKENIZER.encode(x))
         )
 
         self.df = df
-        self.threshold = threshold
         
     def pred_mistranscribed(self, line_numbers: list[int]):
         X_sample = self.df.iloc[line_numbers]['rougeL']
-        return (X_sample > self.threshold).tolist()
+        return (X_sample < self.rougeL_threshold).tolist()
+
 
 class RFDetector(MistranscriptionDetector):
     """
-        Random forest detector. Trained in log_prob_beams_elbowplot.ipynb
+    Random forest detector. Trained in log_prob_beams_elbowplot.ipynb
     """
-    def __init__(
-            self, 
-            **kwargs
-        ):
+    def __init__(self, **kwargs):
 
         defaults = {
             'meeting_path' : "./shared/datasets/amicorpus/train/ES2005d/",
             'model_path' : './model_weights/rf_model.pkl',
-            'threshold' : 0.3583
+            'prob_threshold' : 0.3583 # Random Forest output prob Threshold determined using data analysis
         }
 
         params  = {**defaults, **kwargs}
 
         meeting_path = params['meeting_path']
         model_path = params['model_path']
-        threshold = params['threshold']
+        self.prob_threshold = params['prob_threshold']
 
         # Read the transcript using the provided path
         beam_results = os.path.join(meeting_path, "artifacts", "beam_results.json")
@@ -240,22 +224,24 @@ class RFDetector(MistranscriptionDetector):
         
         df['text'] = df['beam_1_text']
         df['num_tokens_text'] = df['beam_1_text'].apply(
-            lambda x: len(tokenizer.encode(x))
+            lambda x: len(LLM_TOKENIZER.encode(x))
         )
         # df['num_tokens_gt'] = df['gt'].apply(
         #     lambda x: len(tokenizer.encode(x))
         # )
 
         self.df = df
-        self.threshold = threshold
         with open(model_path, 'rb') as file:
             self.model = pickle.load(file)
             self.features = self.model.feature_names_in_
+
         
     def pred_mistranscribed(self, line_numbers: list[int]):
         X_sample = self.df.iloc[line_numbers][self.features]
         pred_probas = self.model.predict_proba(X_sample)[:,1]
-        return (pred_probas > self.threshold).tolist()
+        return (pred_probas >= self.prob_threshold).tolist()
+
+
 
 # Driver code to test implementation
 if __name__ == '__main__':
